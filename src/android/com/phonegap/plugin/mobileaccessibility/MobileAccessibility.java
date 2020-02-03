b4cc472a46dd747a5027294b4499a8e89ca9628b
Roger Solis <roger@myaurea.es>
2020-02-03 16:32:04 +0100

    Removed runOnUiThread for disableDisplayZoom action.

--- src/android/com/phonegap/plugin/mobileaccessibility/MobileAccessibility.java
+++ src/android/com/phonegap/plugin/mobileaccessibility/MobileAccessibility.java
@@ -107,11 +107,12 @@ public class MobileAccessibility extends CordovaPlugin {
                 stop();
                 return true;
             } else if (action.equals("disableDisplayZoom")) {
-                cordova.getActivity().runOnUiThread(new Runnable() {
+                /*cordova.getActivity().runOnUiThread(new Runnable() {
                     public void run() {
                         mMobileAccessibilityHelper.disableDisplayZoom();
                     }
-                });
+                });*/
+                mMobileAccessibilityHelper.disableDisplayZoom();
                 return true;
             }
         } catch (JSONException e) {
